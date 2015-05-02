76545094c4cd3be7e73c9b4dfb986ca0a4ec427f
Ronen Stern <rshtern.fed@gmail.com>
2015-05-03 01:28:04 +0300

    more fixes

--- js/preload.js
+++ js/preload.js
@@ -5,7 +5,12 @@ window.addEventListener('DOMContentLoaded', function() {
         backgroundColor: "#122c45",
         percentage: false,
         barHeight: 1,
+        onComplete: hidePreLoader,
         minimumTime: 200,
         fadeOutTime: 1000
     });
+    console.log(QueryLoader2);
 });
+function hidePreLoader() {
+        $("#preloader").hide();
+    }
