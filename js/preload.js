e4b51ff34246e96101ec5b54da06b4fb4d2cd4fa
Ronen Stern <rshtern.fed@gmail.com>
2015-05-27 21:16:12 +0300

    more updates

--- js/preload.js
+++ js/preload.js
@@ -1,10 +1,10 @@
 window.addEventListener('DOMContentLoaded', function() {
     "use strict";
-    var ql = new QueryLoader2(document.querySelector("section.hero"), {
+    var ql = new QueryLoader2(document.querySelector("section.services"), {
         barColor: "#ee6e73",
         backgroundColor: "#122c45",
         percentage: false,
-        barHeight: 1,
+        barHeight: 2,
         onComplete: hidePreLoader,
         minimumTime: 200,
         fadeOutTime: 1000
